318f7218e7d15543856a027fc4bcd733359ee1d6
sajjadcodes <sajjadcodes@gmail.com>
2024-11-09 11:29:45 +0500

    initialed structure

---
+++ wp-custom-post-type-generator.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Custom Post Type Generator
+Description: Generate custom post types with ease from the admin and frontend.
+Version: 1.0
+Author: Your Name
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class CPTG_Plugin {
+ 
+    }
+
+// Initialize the plugin
+new CPTG_Plugin();
